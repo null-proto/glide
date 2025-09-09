bdbd97ed91202a38d3b19c607cd1b4510608b73f
null-proto <209804848+null-proto@users.noreply.github.com>
2025-09-09 18:14:56 +0530

    fix git backend spawn

--- server/src/git/mod.rs
+++ server/src/git/mod.rs
@@ -1,4 +1,4 @@
-use std::{io::Stdout, net::TcpStream, os::{fd::{AsRawFd, FromRawFd}, unix::process::CommandExt}, process::{Command, Stdio}};
+use std::{net::TcpStream, os::fd::{AsRawFd, FromRawFd}, process::{Command, Stdio}};
 
 
 pub fn http_backend<'a>(
@@ -8,7 +8,8 @@ pub fn http_backend<'a>(
   project_dir: &'static str,
   stream : &mut TcpStream
 ) {
-  let stdio = unsafe { Stdio::from_raw_fd( stream.as_raw_fd()) };
+  let stdin = unsafe { Stdio::from_raw_fd( stream.as_raw_fd()) };
+  let stdout = unsafe { Stdio::from_raw_fd( stream.as_raw_fd()) };
 
   let git = Command::new("git http-backend")
     .env("REQUEST_METHOD", method)
@@ -16,13 +17,27 @@ pub fn http_backend<'a>(
     .env("PATH_INFO", path_info)
     .env("GIT_HTTP_EXPORT_ALL", "1")
     .env("QUERY_STRING", query)
-    .stdout(stdio)
-    .exec();
+    .stdout(stdout)
+    .stdin(stdin)
+    .spawn();
 
-  match git.kind() {
-    std::io::ErrorKind::NotFound => {}
+  match git {
 
     _ => {}
   }
 
+}
+
+
+pub fn create_bare<'a>(
+  path_info: &'a str,
+  project_dir: &'static str,
+) -> Result<() , std::io::Error> {
+
+  let path = format!("{}/{}" ,project_dir , path_info );
+
+  let _ = Command::new(path).spawn()?;
+  let _ = Command::new("git init -m master --bare").spawn()?;
+
+  Ok(())
 }
